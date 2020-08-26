0ecfbecba2562b75327c173f6662c31d1f443e4e
Carina-P <carina.palmqvist@telia.com>
2020-08-26 11:13:22 +0000

    Used fetched information from file to build markers with infoWindow and also cluster

--- assets/js/map.js
+++ assets/js/map.js
@@ -41,35 +41,29 @@ function initMap(){
         return res.json();
     })
     .then((resorts) => {
-        console.log(resorts);
+        let markers = [];
+
+        resorts.forEach((resort) => {
+            let infoWindow = new google.maps.InfoWindow({content: `<p><strong>${resort.name}</strong><br> Altitude base:
+                    ${resort.altitudeB}<br>Altitude top:${resort.altitudeT}<br>Slopes: ${resort.pists}
+                    <br>Number of lifts: ${resort.nrLifts} </p>`
+                });
+
+            let marker = new google.maps.Marker({position:resort.position, map:map, icon: "assets/images/yellow-marker48.gif"});
+        
+            marker.addListener("click", () => {
+                infoWindow.open(map, marker);
+                $("#place-txt").css("background-color","#ffffff");
+                $("#place-txt").html(`<h2>${resort.name}</h2>`);
+            });
+            markers.push(marker);
+        })
+        return markers;
     }) 
-    .catch((error) => console.error("Error:", error))
-
-    var infoWindow = new google.maps.InfoWindow({content: "this is a test"});
-
-    var marker = new google.maps.Marker({position:{lat: 63.399043, lng: 13.081506}, map:map, icon: "assets/images/yellow-marker48.gif"});
-        
-    marker.addListener("click", function(){
-            infoWindow.open(map, marker);
-            $("#place-txt").css("background-color","#ffffff");
-            $("#place-txt").html("<p>Text about Ski resort</p>");
-    });
-    
-    let resorts = [
-        {lat: 45.297309, lng: 6.579732}, 
-        {lat: 46.096081, lng: 7.228551}
-    ];
-
-   
-    let markers = resorts.map(function(resort) {
-          return new google.maps.Marker({
-            position: resort,
-            icon:"assets/images/yellow-marker48.gif"
-          });
-        });
-
-        
-    let markerCluster = new MarkerClusterer(map, markers, {imagePath: 'assets/images/m'});
-
+    .then((markers) => {
+        let markerCluster = new MarkerClusterer(map, markers, {imagePath: 'assets/images/m'});  
+    })
+    .catch((error) => console.error("Error:", error)); 
+ 
     getResortInfo();
 }
