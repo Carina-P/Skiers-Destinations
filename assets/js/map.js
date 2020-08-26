4583ec9e5ce1556bd69c3b6186b91a2368f1ca45
Carina-P <carina.palmqvist@telia.com>
2020-08-26 09:20:07 +0000

    Added a map

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap(){
+    
+    map = new google.maps.Map(document.getElementById("map"), 
+        {
+            zoom: 3,
+            center: {lat: 45.297309, lng: 6.579732}
+        }
+    );
+}
