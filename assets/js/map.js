7d2b5ec1897dd5cc6c48f98c2f37e559af33171e
Carina-P <carina.palmqvist@telia.com>
2020-08-26 11:32:09 +0000

    Rearranged so all promises are chained and in forEach I have a promise.all - when fetching information from file with resorts and then for each resort: two fetches from weather unlocked

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,27 @@
-function getResortInfo(){
-    let resortId = 512001;
-    fetch(`https://api.weatherunlocked.com/api/snowreport/${resortId}?app_id=754144cc&app_key=108769d13601e41f8dfeb934ee961859`)
+let map;
+
+function buildMarker(resort){  
+     
+    let infoWindow = new google.maps.InfoWindow({content: `<p><strong>${resort.name}</strong><br> Altitude base:
+                    ${resort.altitudeB}<br>Altitude top:${resort.altitudeT}<br>Slopes: ${resort.pists}
+                    <br>Number of lifts: ${resort.nrLifts} </p>`});
+            
+    let marker = new google.maps.Marker({position: resort.position, icon:"assets/images/yellow-marker48.gif"});
+            
+    marker.addListener("click", () => {
+        infoWindow.open(map, marker);
+        $("#place-txt").css("background-color","#ffffff");
+        $("#place-txt").html(`<h2>${resort.name}</h2>
+            <p>${resort.info}</p>`);
+    });
+    return marker;
+}
+
+function getResortInfo(resort) {
+    /* let snowRep;
+    let weatherRep;
+    */ 
+    fetch(`https://api.weatherunlocked.com/api/snowreport/${resort.id}?app_id=754144cc&app_key=108769d13601e41f8dfeb934ee961859`)
     .then((snowRes) => {
         if (!snowRes.ok){
             throw new Error("Something is wrong fetching snowreport");
@@ -8,8 +29,9 @@ function getResortInfo(){
         return snowRes.json();
     })
     .then ((snowReport) => {
-        console.log(snowReport)
-        return (fetch(`https://api.weatherunlocked.com/api/resortforecast/${resortId}?hourly_interval=6&app_id=754144cc&app_key=108769d13601e41f8dfeb934ee961859`))
+            /* snowRep = snowReport; */
+        resort.snowReport = snowReport;
+        return (fetch(`https://api.weatherunlocked.com/api/resortforecast/${resort.id}?hourly_interval=6&app_id=754144cc&app_key=108769d13601e41f8dfeb934ee961859`))
     })
     .then( (weatherRes) => {
         if (!weatherRes.ok){
@@ -18,21 +40,17 @@ function getResortInfo(){
         return weatherRes.json();
     })
     .then ((weather) => {
-        console.log(weather);
+        /* weatherRep = weather */
+        resort.forecast = weather.forecast;
     })
     .catch((error) => {console.error("error: ", error) });
- 
-}
 
-function initMap(){
     
-    let map = new google.maps.Map(document.getElementById("map"), 
-        {
-            zoom: 3,
-            center: {lat: 45.297309, lng: 6.579732}
-        }
-    );
+    return buildMarker(resort);
+    /*return buildMarker(resort, snowRep, weatherRep); */
+} 
 
+function makeMarkersCluster(){ 
     fetch("assets/data/resorts.json")
     .then((res) => {
         if (!res.ok){
@@ -41,29 +59,22 @@ function initMap(){
         return res.json();
     })
     .then((resorts) => {
-        let markers = [];
-
-        resorts.forEach((resort) => {
-            let infoWindow = new google.maps.InfoWindow({content: `<p><strong>${resort.name}</strong><br> Altitude base:
-                    ${resort.altitudeB}<br>Altitude top:${resort.altitudeT}<br>Slopes: ${resort.pists}
-                    <br>Number of lifts: ${resort.nrLifts} </p>`
-                });
-
-            let marker = new google.maps.Marker({position:resort.position, map:map, icon: "assets/images/yellow-marker48.gif"});
-        
-            marker.addListener("click", () => {
-                infoWindow.open(map, marker);
-                $("#place-txt").css("background-color","#ffffff");
-                $("#place-txt").html(`<h2>${resort.name}</h2>`);
-            });
-            markers.push(marker);
-        })
-        return markers;
+        return Promise.all( resorts.map( getResortInfo));
     }) 
-    .then((markers) => {
-        let markerCluster = new MarkerClusterer(map, markers, {imagePath: 'assets/images/m'});  
-    })
-    .catch((error) => console.error("Error:", error)); 
- 
-    getResortInfo();
+    .then ((markers) => {
+        new MarkerClusterer(map, markers, {imagePath: 'assets/images/m'});
+    }) 
+    .catch((error) => console.error("Error:", error))
+}
+
+
+function initMap(){
+    
+    map = new google.maps.Map(document.getElementById("map"), 
+        {
+            zoom: 3,
+            center: {lat: 45.297309, lng: 6.579732}
+        }
+    );
+    makeMarkersCluster();
 }
