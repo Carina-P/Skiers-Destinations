77fcfc12b8d399c1dda355bda8b4a1d16374bec2
Carina-P <carina.palmqvist@telia.com>
2020-08-26 10:06:28 +0000

    One yellow marker in the map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,23 @@
 function initMap(){
     
-    map = new google.maps.Map(document.getElementById("map"), 
+    let map = new google.maps.Map(document.getElementById("map"), 
         {
             zoom: 3,
             center: {lat: 45.297309, lng: 6.579732}
         }
     );
+
+    var marker = new google.maps.Marker({position: {lat: 45.297309, lng: 6.579732}, map: map,
+    icon: "assets/images/yellow-marker48.gif" });
+
+    /*
+    let markers = locations.map(function(location) {
+          return new google.maps.Marker({
+            position: location,
+            icon:"assets/images/yellow-marker48.gif"
+          });
+        });
+    console.log(markers);
+        
+    let markerCluster = new MarkerClusterer(map, markers, {imagePath: 'assets/images/m'});*/
 }
