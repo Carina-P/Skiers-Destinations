1a64969d978a2d855c76b5c1bdb7e00ef7261640
Carina-P <carina.palmqvist@telia.com>
2020-08-26 10:33:34 +0000

    Fetch information from weather unlocked for Åre

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,17 @@
+function getResortInfo(){
+    let resortId = 512001;
+    fetch(`https://api.weatherunlocked.com/api/snowreport/${resortId}?app_id=754144cc&app_key=108769d13601e41f8dfeb934ee961859`)
+    .then((snowRes) => {
+        if (!snowRes.ok){
+            throw new Error("Something is wrong fetching snowreport");
+        }
+        return snowRes.json();
+    })
+    .then ((snowReport) => {
+        console.log(snowReport);
+    })
+}
+
 function initMap(){
     
     let map = new google.maps.Map(document.getElementById("map"), 
@@ -32,4 +46,6 @@ function initMap(){
 
         
     let markerCluster = new MarkerClusterer(map, markers, {imagePath: 'assets/images/m'});
+
+    getResortInfo();
 }
