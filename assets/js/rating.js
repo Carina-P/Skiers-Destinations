b0bab17035c69299c0ad67ccc6e2c4fd29b4238c
Carina-P <carina.palmqvist@telia.com>
2020-08-30 18:07:11 +0000

    Started all over with rating.js. Added initial function.

--- assets/js/rating.js
+++ assets/js/rating.js
@@ -1,10 +1,42 @@
-function calcNewRating(event) {
-    console.log("test", event.data.index, this.value);
-    let txtHTML = ``;
-    for (let i=0; i<this.value; i++){
-        txtHTML += `<span class="fas fa-star yellow"></span>`;
-    }
-    $("#row1").html(txtHTML + `  ${this.value}`);
-}
+/** 
+* Use if no rating information is available in local storage. Initial 
+* information consists of Ski resorts but no rating information.
+*/
+function initTable(){ 
+    console.log("initTable");
+    ratingTable = [
+        {
+            name: "Val Thorens",
+            rating: 0.0,
+            nrOfVotes: 0 
+        }
+    ];
 
-$("#grades1").change({index : 1} , calcNewRating );
+    console.log(ratingTable);
+    return ratingTable;
+} 
+ 
+/**
+* Users can cast a vote for each resort every session. This value is not saved in local storage.
+* Thus the vote is set to 0 every time a new browser session starts. 
+*/
+function initMyVote(row){
+    console.log("initMyVote"); 
+} 
+/*
+* Update/fill document with rating information.
+*/
+function fillDocuTable(){
+    console.log("fillDocuTable");
+}
+/** 
+* Fetch information about rating and then fill document with the information.
+* User can cast new votes every session.
+*/
+function fetchTableData(){
+    let ratingTable = localStorage.getItem("ratingTable") || initTable();
+    ratingTable.forEach(initMyVote);
+    fillDocuTable();
+} 
+
+$(document).ready(fetchTableData); 
