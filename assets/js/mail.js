1181f4760f4913a11b37f12b54186354d8bf9ce4
Carina-P <carina.palmqvist@telia.com>
2020-09-01 16:22:32 +0000

    Resets the form when email is sent

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -7,6 +7,7 @@ function sendMail(contactForm){
     })
     .then( (response) => {
             $("#mailOK").modal();
+            $("#mailForm").trigger("reset");
         },
         (error) => {
             $("mailFail").modal();
