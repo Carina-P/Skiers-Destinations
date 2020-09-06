8a17e250c23e0e78c94e5b80444998eba1aa57ad
Carina-P <carina.palmqvist@telia.com>
2020-09-06 14:22:21 +0000

    Done doing rating more object oriented

--- assets/js/rating.js
+++ assets/js/rating.js
@@ -1,3 +1,6 @@
+/** 
+ * Constructor function for RatedResort
+ */
 function RatedResort( name, rating, nrOfVotes, lastVote){
     this.name = name;
     this.rating = rating;
@@ -7,14 +10,14 @@ function RatedResort( name, rating, nrOfVotes, lastVote){
     this.getRating = () => this.rating;
     this.setLastVote = (vote) => {this.lastVote = vote};
  
-/** 
-* Returns a string literal of HTML code that contains stars representing the
-* value of the input parameter.
-*
-* @param {number} grade Value to be represented by stars in HTML code.
-*
-* @returns {literal} HTML code
-*/
+    /** 
+     * Returns a string literal of HTML code that contains stars representing the
+     * value of the input parameter.
+     *
+     * @param {number} grade Value to be represented by stars in HTML code.
+     *
+     * @returns {literal} HTML code
+     */
     this.starsToHTML = (grade) => {
         let starsHTML =``;
         let fullStars = 0;
@@ -42,13 +45,13 @@ function RatedResort( name, rating, nrOfVotes, lastVote){
  
         return starsHTML;    
     } 
-/** 
- * Returns HTML code with scrolldown of numbers 1 to 5. 
- * 
- * @param {string} id Place in DOM to insert the HTML code.
- * 
- * @returns {literal} HTML code
- */
+    /** 
+     * Returns HTML code with scrolldown of numbers 1 to 5. 
+     * 
+     * @param {string} id Place in DOM to insert the HTML code.
+     * 
+     * @returns {literal} HTML code
+     */
     this.noVoteHTML = (id) => {
         let voteHTML = `<label for = ${id}>My grade:</label>
                     <select name = ${id} id=${id}>
@@ -60,13 +63,13 @@ function RatedResort( name, rating, nrOfVotes, lastVote){
         voteHTML += `</select>`;
     return  voteHTML;
     }
-/**
-* HTML code for a this resort with rating.
-*
-* @param {number} rowIndex Indicates were in a table this information belongs 
-*
-* @returns {string} HTLM code
-*/ 
+    /**
+    * HTML code for a this resort with rating.
+    *
+    * @param {number} rowIndex Indicates were in a table this information belongs 
+    *
+    * @returns {string} HTLM code
+    */ 
      this.rowToHTML = (rowIndex) => {
         let rowHTML = `<td>${rowIndex+1}</td>
                         <td>${this.name}</td>
@@ -93,11 +96,11 @@ function RatedResort( name, rating, nrOfVotes, lastVote){
     
      return rowHTML;
     } 
-/**
- * Put information about this resorts rating in the document.
- * 
- * @param {number} rowIndex Indicates where in a table information belongs
- */
+    /**
+     * Put information about this resorts rating in the document.
+     * 
+     * @param {number} rowIndex Indicates where in a table information belongs
+     */
     this.toDocument = (rowIndex) => {
         let id = "#index"+rowIndex;
         $(id).html(this.rowToHTML(rowIndex));
@@ -107,11 +110,11 @@ function RatedResort( name, rating, nrOfVotes, lastVote){
                 ratedList.updateList);
         }
     }
-/**
-* Update rating of resort with the last vote
-*
-* @param {number} vote The value of a new vote to be added to the rating.
-*/
+    /**
+    * Update rating of resort with the last vote
+    *
+    * @param {number} vote The value of a new vote to be added to the rating.
+    */
     this.calulateNewRating = (vote) => {
     
         if (typeof(this.rating)!="number" || typeof(this.nrOfVotes)!="number" || typeof(vote)!="number"){
@@ -122,4 +125,67 @@ function RatedResort( name, rating, nrOfVotes, lastVote){
         this.rating = (this.rating*this.nrOfVotes++ +vote)/(this.nrOfVotes);
         this.lastVote = vote; 
     }
-} 
+}
+
+/** 
+ * Constructor function for RatedResort
+ */
+function RatedList(){
+    this.fetchInitList = () => {
+        let list =[];
+        list.push(new RatedResort("Bad Gastein", 0.0, 0, 0));
+        list.push(new RatedResort("Charmonix", 0.0, 0, 0));
+        list.push(new RatedResort("Cortina d'Ampesso", 0.0, 0, 0));
+        list.push(new RatedResort("Trysil", 0.0, 0, 0));
+        list.push(new RatedResort("Val d'Isere", 0.0, 0, 0));
+        list.push(new RatedResort("Val Thorens", 0.0, 0, 0));
+        list.push(new RatedResort("Verbier", 0.0, 0, 0));
+        list.push(new RatedResort("Zermatt", 0.0, 0, 0));
+        list.push(new RatedResort("Zugspitze", 0.0, 0, 0));
+        list.push(new RatedResort("Åre", 0.0, 0, 0));
+    
+        return list;
+    }
+    /**
+     * Get lists values form localStorage or if not in localStorage starts 
+     * a new list.
+     * 
+     * @returns {Object} An array of RatedResort
+     */
+    this.getList =  () => {
+        let list = [];
+        let table = JSON.parse(localStorage.getItem("ratingTable"));
+        if (table === null) {return this.fetchInitList();}
+        
+        table.forEach(item => {list.push(new RatedResort(item.name, item.rating, item.nrOfVotes, 0))} );
+        return list;
+    }
+    
+    this.list = this.getList();
+    /**
+     * Put the information in the RatedList into the document.
+     */
+    this.toDocument = () => {
+        this.list.forEach( (ratedResort, index) => { ratedResort.toDocument(index);});
+    }
+    /**
+     * Save information to localStorage.
+     */
+    this.toLocalStorage = () => {
+        let table = this.list; 
+        localStorage.setItem("ratingTable", JSON.stringify(table)); 
+    }
+    /**
+     * Updates RatingList when a new vote is entered
+     */
+    this.updateList = (event) => {
+        this.list[event.data.index].calulateNewRating( parseInt(event.target.value));
+        this.list.sort((resortA,resortB) => {return resortB.getRating()-resortA.getRating();}); 
+        ratedList.toDocument();
+        ratedList.toLocalStorage();
+    }
+}
+
+let ratedList = new RatedList();
+
+$(document).ready(ratedList.toDocument());
