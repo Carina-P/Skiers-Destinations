10e8af906932858e103f960fc642c5918cd69477
Carina-P <carina.palmqvist@telia.com>
2020-09-06 13:53:04 +0000

    Added function rowToHTML to RatedResorts

--- assets/js/rating.js
+++ assets/js/rating.js
@@ -12,6 +12,8 @@ function RatedResort( name, rating, nrOfVotes, lastVote){
 * value of the input parameter.
 *
 * @param {number} grade Value to be represented by stars in HTML code.
+*
+* @returns {literal} HTML code
 */
     this.starsToHTML = (grade) => {
         let starsHTML =``;
@@ -39,5 +41,57 @@ function RatedResort( name, rating, nrOfVotes, lastVote){
         } 
  
         return starsHTML;    
+    } 
+/** 
+ * Returns HTML code with scrolldown of numbers 1 to 5. 
+ * 
+ * @param {string} id Place in DOM to insert the HTML code.
+ * 
+ * @returns {literal} HTML code
+ */
+    this.noVoteHTML = (id) => {
+        let voteHTML = `<label for = ${id}>My grade:</label>
+                    <select name = ${id} id=${id}>
+                        <option value=0></option>`;
+        for (let i=5; i>0; i--){
+            voteHTML += `<option value=${i}>${i}</option>`;
+        }    
+
+        voteHTML += `</select>`;
+    return  voteHTML;
     }
+/**
+* HTML code for a this resort with rating.
+*
+* @param {number} rowIndex Indicates were in a table this information belongs 
+*
+* @returns {string} HTLM code
+*/
+
+     this.rowToHTML = (rowIndex) => {
+        let rowHTML = `<td>${rowIndex+1}</td>
+                        <td>${this.name}</td>
+                        <td>`;
+
+        rowHTML += this.starsToHTML(this.rating);
+        rowHTML += `<span> ${this.rating.toFixed(2)}</span>`;
+
+        rowHTML += `</td> 
+                     <td> 
+                        <div>`;
+
+        if (this.lastVote == 0) {
+            let id = "gradeIndex" + rowIndex;
+            rowHTML += this.noVoteHTML(id);  
+        }
+        else {
+            rowHTML += this.starsToHTML(this.lastVote);
+            rowHTML += `<span> ${this.lastVote}</span>`;
+        }
+
+        rowHTML += `   </div>
+                        </td>`;
+    
+     return rowHTML;
+    } 
 } 
