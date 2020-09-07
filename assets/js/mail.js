6d1f8ea424d5cae6add0e6194a3bd4320266b594
Carina-P <carina.palmqvist@telia.com>
2020-09-07 13:43:09 +0000

    Make respons if mail is sent OK or not, show in same modal

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,5 +1,6 @@
 function sendMail(contactForm){
-    $("#mail-sending").html(`<img src="assets/css/loader.gif" alt="loading..." /> <span>sending mail...</span>`);
+    $("#mail-sending").html(`<img src="assets/css/loader.gif" alt="loading..."/> 
+                            <span>sending mail...</span>`);
     emailjs.send("gmail","skiers_destinations",{
         "resort": contactForm.resort.value,
         "reason": contactForm.reason.value,
@@ -7,12 +8,19 @@ function sendMail(contactForm){
         "email": contactForm.email.value
     })
     .then( () => {
-            $("#mailOK").modal();
+            $("#mail-feedback").modal();
+            $(".modal-title").html(`<strong>Your email is sent</strong>`);
+            $(".modal-body").html(`<p>We will read it during the day.</p> 
+                            <p>Thank you for contributing to this page!</p>`)
             $("#mailForm").trigger("reset");
             $("#mail-sending").html(``);
         },
         (error) => {
-            $("#mailFail").modal();
+            $("#mail-feedback").modal();
+            $(".modal-title").html(`<strong>Error</strong>`);
+            $(".modal-body").html(`<p>Unfortunately we could not send the mail.</p> 
+                                    <p>Please try again!</p>`)
+            $("#mail-sending").html(``);
             console.log(error);
         }
     );
