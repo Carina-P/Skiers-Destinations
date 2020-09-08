af34dd864423799f98199f947032286c80604c8e
Carina-P <carina.palmqvist@telia.com>
2020-09-08 05:43:10 +0000

    Fixed bug with reseting the form

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,30 +1,23 @@
-function sendMail(contactForm){
+$("#mail-form").submit( () => {
     $("#mail-sending").html(`<img src="assets/css/loader.gif" alt="loading..."/> 
                             <span>sending mail...</span>`);
-    emailjs.send("gmail","skiers_destinations",{
-        "resort": contactForm.resort.value,
-        "reason": contactForm.reason.value,
-        "name": contactForm.name.value,
-        "email": contactForm.email.value
-    })
+    emailjs.sendForm("gmail","skiers_destinations", "#mail-form") 
     .then( () => {
-            $("#mail-feedback").modal();
-            $(".modal-title").html(`<strong>Your email is sent</strong>`);
-            $(".modal-body").html(`<p>We will read it during the day.</p> 
-                            <p>Thank you for contributing to this page!</p>`)
-            $("#mailForm").trigger("reset");
-            $("#mail-sending").html(``);
-        },
-        (error) => {
-            $("#mail-feedback").modal();
-            $(".modal-title").html(`<strong>Error</strong>`);
-            $(".modal-body").html(`<p>Unfortunately we could not send the mail.</p> 
-                                    <p>Please try again!</p>`)
-            $("#mail-sending").html(``);
-            console.log(error);
-        }
-    );
+        $("#mail-feedback").modal();
+        $(".modal-title").html(`<strong>Your email is sent</strong>`);
+        $(".modal-body").html(`<p>We will read it during the day.</p> 
+                            <p>Thank you for contributing to this page!</p>`);
+        $("#mail-form").trigger("reset");
+        $("#mail-sending").html(``);
+    }, (error) => {
+        $("#mail-feedback").modal();
+        $(".modal-title").html(`<strong>Error</strong>`);
+        $(".modal-body").html(`<p>Unfortunately we could not send the mail.</p> 
+                                    <p>Please try again!</p>`);
+        $("#mail-sending").html(``);
+        console.log(error);
+    });
     return false;
-}
+});
 
-$(function(){emailjs.init("user_cnNZR4MUEsDbHZ4M6sFAo");})
+emailjs.init("user_cnNZR4MUEsDbHZ4M6sFAo");
